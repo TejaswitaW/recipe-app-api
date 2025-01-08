24ec4b00916b8a944ac08355bacb1369ffbfbb87
Tejaswita <tejaswitawakhure@gmail.com>
2025-01-08 18:31:03 +0530

    Add comments for understanding

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -13,12 +13,14 @@ class CommandTests(SimpleTestCase):
     """Test Commannds."""
     def test_wait_for_db_ready(self,patched_check):
         """Test waiting for database if database ready."""
+        # patched_check in your test code is a mock object created by the @patch decorator, 
+        # for the check method in the wait_for_db command
         # when we call check inside our command,inside our
         # test case ,we just want to return the True value
         patched_check.return_value = True
         # execute the inside wait_for_db
         call_command('wait_for_db')
-
+        # It is used to assert that a mocked method or function was called exactly once with a specific set of arguments. 
         patched_check.assert_called_once_with(databases=['default'])
 
     # what should happen when database isnt ready
@@ -34,4 +36,5 @@ class CommandTests(SimpleTestCase):
 
         # checking count of mock method
         self.assertEqual(patched_check.call_count,6)
+        # to assert that a mock object (e.g., a method or function) was called at least once with the specified arguments
         patched_check.assert_called_with(databases=['default'])
